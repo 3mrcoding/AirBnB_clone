4a629898d6189fd77f7aab73cc6a69dd3bfc0dab
Al7assan <al7assan.r@gmail.com>
2023-12-09 22:51:36 +0200

    c 4.0

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,7 @@ class FileStorage:
     # self.__objects[key] = obj.to_dict()
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
+        """A Set in __objects obj with key <obj_class_name>.id"""
         ocname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
